42fc744e97119cfc325b26f60606382317cddc46
Phil Lello <phil@dunlop-lello.uk>
2016-06-11 13:37:19 +0100

    Revert "Site checkin for project Doxygen Maven Report"
    This reverts commit 3d5ece8f6b236a1e29ac92d2723d45ff6bf7f099 which should have gone to gh-pages branch.
    
    Thanks @io7m for highlighting this!

---
+++ src/main/java/com/pcl_solutions/maven/reports/DoxygenReportMojo.java
@@ -0,0 +1,47 @@
+package com.pcl_solutions.maven.reports;
+
+import java.io.File;
+
+/**
+ * @author Phil Lello
+ * 
+ * @goal doxygen
+ * @phase site
+ */
+public class DoxygenReportMojo extends AbstractDoxygenReportMojo {
+
+	public boolean isAggregate() {
+		return false;
+	}
+
+	@Override
+	public void configureInputs() {
+		getLog().debug("Configuring inputs for single report");
+
+		if (getInputs() != null && getInputs().length > 0) {
+			StringBuilder doxygenInputs = new StringBuilder();
+			String root = getBaseDir();
+			if (!root.endsWith("/") && !root.endsWith("\\")) {
+				root = root + File.separator;
+			}
+
+			for (String input : getInputs()) {
+				String fullInput = root + input;
+				getLog().debug("Adding input: " + fullInput);
+				doxygenInputs.append("\"");
+				doxygenInputs.append(fullInput);
+				doxygenInputs.append("\"");
+				doxygenInputs.append(" \\");
+				doxygenInputs.append("\n");
+			}
+
+			String fullInputs = doxygenInputs.substring(0,
+					doxygenInputs.length() - " /\n".length());
+
+			getOptions().put("INPUT", fullInputs);
+		} else {
+			getOptions().put("INPUT", "\"" + getSourceDirectory() + "\"");
+		}
+	}
+
+}
